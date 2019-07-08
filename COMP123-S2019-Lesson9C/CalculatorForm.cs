d51bcc79af24bdf377ad8387648f91c1374d2735
301034220 <301034220@PR-E216-52>
2019-07-08 16:28:08 -0400

    added basic code to convert input to interger

--- COMP123-S2019-Lesson9C/CalculatorForm.cs
+++ COMP123-S2019-Lesson9C/CalculatorForm.cs
@@ -12,9 +12,34 @@ namespace COMP123_S2019_Lesson9C
 {
     public partial class CalculatorForm : Form
     {
+        /// <summary>
+        /// This is the constructor for the CalculatorForm
+        /// </summary>
         public CalculatorForm()
         {
             InitializeComponent();
+        }
+        /// <summary>
+        /// This is a shared event handler for the CalculatorButton click event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void CalculatorButton_Click(object sender, EventArgs e)
+        {
+            //var TheButton = (Button)sender;
+            var TheButton = sender as Button;
+
+            int ButtonValue;
+            bool Result = int.TryParse(TheButton.Text, out ButtonValue);
+
+            if(Result)
+            {
+                ResultLabel.Text = TheButton.Text;
+            }
+            else
+            {
+                ResultLabel.Text = "Not a Number (NAN)";
+            }
         }
     }
 }
